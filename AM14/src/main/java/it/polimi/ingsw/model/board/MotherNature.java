3c4d0a5f72d7b9b04fc5d617b078b65e0e3dad5f
darioamadori <dario.amadori@mail.polimi.it>
2022-03-22 13:39:41 +0100

    First impolementation MotherNature

--- AM14/src/main/java/it/polimi/ingsw/model/board/MotherNature.java
+++ AM14/src/main/java/it/polimi/ingsw/model/board/MotherNature.java
@@ -1,5 +1,33 @@
 package it.polimi.ingsw.model.board;
 
-public class MotherNature {
+/**
+ * This class is used to model the character of Mother Nature
+ */
 
+public class MotherNature {
+    //Attributes
+    private int position;
+
+    /**
+     * Constructor method for the MotherNature Class
+     *
+     * @param position  Integer chosen by players that represents the position of MotherNature in the group of island
+     *
+     */
+    public MotherNature(int position) {
+        this.position = position;
+    }
+
+    /**
+     * This method moves MotherNature
+     *
+     * @param moves Number of movements of motherNature
+     */
+    public void move(int moves, int numIslands){
+        position = (position + moves) % numIslands;
+    }
+
+    public int getPosition() {
+        return position;
+    }
 }
