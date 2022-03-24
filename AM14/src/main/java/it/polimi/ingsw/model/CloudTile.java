9ab667896c4531e184ec6f0890b771d61ebc8a75
darioamadori <dario.amadori@mail.polimi.it>
2022-03-24 16:55:12 +0100

    ClodTile class

--- AM14/src/main/java/it/polimi/ingsw/model/CloudTile.java
+++ AM14/src/main/java/it/polimi/ingsw/model/CloudTile.java
@@ -1,5 +1,56 @@
 package it.polimi.ingsw.model;
 
+import it.polimi.ingsw.helpers.Color;
+import it.polimi.ingsw.helpers.Constants;
+
+/**
+ * CloudTile class is the entity used to make the Cloud
+ * The number of clouds is equals to the numbers of players
+ */
 public class CloudTile {
+
+  //Attributes
+  private int[] students = new int[Constants.getNColors()];
+  private final int id;
+
+  /**
+   * Constructor method:
+   * @param id Unique identifier for each Cloud
+   * students[] initialized to 0
+   */
+  public CloudTile(int id) {
+    this.id = id;
+    emptyCloud();
+  }
+
+  /**
+   * addStudent method add ONE student on the CloudTile
+   * @param color identify the array position to add a student
+   */
+  public void addStudent(int color) {
+    students[color]++;
+  }
+
+  /**
+   * emptyCloud is used to empty the CloudTile,
+   * it returns the array students and sets it to 0
+   *
+   * @return an array of students
+   */
+  public int[] emptyCloud() {
+    int[] studentsToReturn = students;
+    for(Color color : Color.values()) {
+      students[color.ordinal()] = 0;
+    }
+    return studentsToReturn;
+  }
+
+  public int[] getStudents() {
+    return students;
+  }
+
+  public int getId() {
+    return id;
+  }
 
 }
