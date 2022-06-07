0834f39b892d2c5df12b11210944ce5c06f70600
nixetheus <luca.cassenti.00@gmail.com>
2022-06-07 22:08:17 +0200

    Other 2 controllers with test coverage

--- src/test/java/it/polimi/ingsw/model/controller/MoveControllerTest.java
+++ src/test/java/it/polimi/ingsw/model/controller/MoveControllerTest.java
@@ -1,5 +1,169 @@
 package it.polimi.ingsw.model.controller;
 
+import it.polimi.ingsw.controller.MoveController;
+import it.polimi.ingsw.controller.PlayController;
+import it.polimi.ingsw.helpers.Color;
+import it.polimi.ingsw.helpers.MessageSecondary;
+import it.polimi.ingsw.helpers.Towers;
+import it.polimi.ingsw.messages.MoveMessage;
+import it.polimi.ingsw.messages.MoveMessageResponse;
+import it.polimi.ingsw.messages.PlayMessage;
+import it.polimi.ingsw.messages.PlayMessageResponse;
+import it.polimi.ingsw.model.Game;
+import it.polimi.ingsw.model.Team;
+import it.polimi.ingsw.model.player.Player;
+import java.io.IOException;
+import java.util.Vector;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class MoveControllerTest {
+
+  @Test
+  public void moveMotherNatureTest() throws IOException {
+
+    Vector<Team> teams = new Vector<>();
+    Team team1 = new Team(0, Towers.BLACK);
+    Team team2 = new Team(1, Towers.WHITE);
+
+    Player player1 = new Player(0, "test1");
+    player1.playAssistant((int) (Math.random() * 10));
+    team1.addPlayer(player1);
+    Player player2 = new Player(0, "test2"); team1.addPlayer(player2);
+    teams.add(team1); teams.add(team2);
+
+    Game testGame = new Game();
+    testGame.setTeams(teams);
+    testGame.setExpert(false);
+
+    MoveController moveControllerTest = new MoveController();
+    MoveMessage moveMessageTest = new MoveMessage(MessageSecondary.MOVE_MN);
+
+    int moves = (int) (Math.random() *  player1.getAssistant().getMoves());
+    int islandId = (testGame.getMainBoard().getMotherNature().getPosition()
+        + moves) % testGame.getMainBoard().getIslands().size();
+    moveMessageTest.setIslandNumber(islandId);
+
+    MoveMessageResponse response =
+        (MoveMessageResponse) moveControllerTest.elaborateMessage(moveMessageTest, testGame);
+    Assert.assertEquals(response.getMessageSecondary(), MessageSecondary.MOVE_MN);
+    Assert.assertEquals(response.getPlayerId(), player1.getPlayerId());
+    Assert.assertEquals(testGame.getMainBoard().getMotherNature().getPosition(), islandId);
+  }
+
+  @Test
+  public void moveMotherNatureNegativeTest() throws IOException {
+
+    Vector<Team> teams = new Vector<>();
+    Team team1 = new Team(0, Towers.BLACK);
+    Team team2 = new Team(1, Towers.WHITE);
+
+    Player player1 = new Player(0, "test1");
+    player1.playAssistant((int) (Math.random() * 10));
+    team1.addPlayer(player1);
+    Player player2 = new Player(0, "test2"); team1.addPlayer(player2);
+    teams.add(team1); teams.add(team2);
+
+    Game testGame = new Game();
+    testGame.setTeams(teams);
+    testGame.setExpert(false);
+
+    MoveController moveControllerTest = new MoveController();
+    MoveMessage moveMessageTest = new MoveMessage(MessageSecondary.MOVE_MN);
+
+    int moves = player1.getAssistant().getMoves() + 1;
+    int islandId = (testGame.getMainBoard().getMotherNature().getPosition()
+        + moves) % testGame.getMainBoard().getIslands().size();
+    moveMessageTest.setIslandNumber(islandId);
+
+    MoveMessageResponse response =
+        (MoveMessageResponse) moveControllerTest.elaborateMessage(moveMessageTest, testGame);
+    Assert.assertNull(response);
+  }
+
+  @Test
+  public void moveCloudTileTest() throws IOException {
+
+    Vector<Team> teams = new Vector<>();
+    Team team1 = new Team(0, Towers.BLACK);
+    Team team2 = new Team(1, Towers.WHITE);
+
+    Player player1 = new Player(0, "test1"); team1.addPlayer(player1);
+    Player player2 = new Player(0, "test2"); team1.addPlayer(player2);
+    teams.add(team1); teams.add(team2);
+
+    Game testGame = new Game();
+    testGame.setTeams(teams);
+    testGame.setExpert(false);
+
+    MoveController moveControllerTest = new MoveController();
+    MoveMessage moveMessageTest = new MoveMessage(MessageSecondary.CLOUD_TILE);
+
+    int cloudId = (int) (Math.random() * testGame.getCloudTiles().size());
+    moveMessageTest.setCloudTileNumber(cloudId);
+
+    MoveMessageResponse response =
+        (MoveMessageResponse) moveControllerTest.elaborateMessage(moveMessageTest, testGame);
+    Assert.assertEquals(response.getMessageSecondary(), MessageSecondary.CLOUD_TILE);
+    Assert.assertEquals(response.getPlayerId(), player1.getPlayerId());
+    Assert.assertEquals(response.getCloudTileNumber(), cloudId);
+  }
+
+  @Test
+  public void moveEntranceDiningRoomTest() throws IOException {
+
+    Vector<Team> teams = new Vector<>();
+    Team team1 = new Team(0, Towers.BLACK);
+    Team team2 = new Team(1, Towers.WHITE);
+
+    Player player1 = new Player(0, "test1"); team1.addPlayer(player1);
+    Player player2 = new Player(0, "test2"); team1.addPlayer(player2);
+    teams.add(team1); teams.add(team2);
+
+    Game testGame = new Game();
+    testGame.setTeams(teams);
+    testGame.setExpert(false);
+
+    MoveController moveControllerTest = new MoveController();
+    MoveMessage moveMessageTest = new MoveMessage(MessageSecondary.ENTRANCE);
+
+    int index = 0;
+    while (player1.getPlayerBoard().getEntrance().getStudents()[index++] <= 0);
+
+    moveMessageTest.setStudentColor(index);
+
+    MoveMessageResponse response =
+        (MoveMessageResponse) moveControllerTest.elaborateMessage(moveMessageTest, testGame);
+    Assert.assertEquals(response.getMessageSecondary(), MessageSecondary.MOVE_STUDENT_ENTRANCE);
+  }
+
+  @Test
+  public void moveEntranceIslandTest() throws IOException {
+
+    Vector<Team> teams = new Vector<>();
+    Team team1 = new Team(0, Towers.BLACK);
+    Team team2 = new Team(1, Towers.WHITE);
+
+    Player player1 = new Player(0, "test1"); team1.addPlayer(player1);
+    Player player2 = new Player(0, "test2"); team1.addPlayer(player2);
+    teams.add(team1); teams.add(team2);
+
+    Game testGame = new Game();
+    testGame.setTeams(teams);
+    testGame.setExpert(false);
+
+    MoveController moveControllerTest = new MoveController();
+    MoveMessage moveMessageTest = new MoveMessage(MessageSecondary.ENTRANCE);
+
+    int index = 0;
+    while (player1.getPlayerBoard().getEntrance().getStudents()[index++] <= 0);
+
+    moveMessageTest.setStudentColor(index);
+    moveMessageTest.setPlace(1);
+
+    MoveMessageResponse response =
+        (MoveMessageResponse) moveControllerTest.elaborateMessage(moveMessageTest, testGame);
+    Assert.assertEquals(response.getMessageSecondary(), MessageSecondary.MOVE_STUDENT_ENTRANCE);
+  }
 
 }
--- src/test/java/it/polimi/ingsw/model/controller/PlayControllerTest.java
+++ src/test/java/it/polimi/ingsw/model/controller/PlayControllerTest.java
@@ -1,9 +1,117 @@
 package it.polimi.ingsw.model.controller;
 
+import it.polimi.ingsw.controller.PlayController;
+import it.polimi.ingsw.helpers.Color;
+import it.polimi.ingsw.helpers.MessageSecondary;
+import it.polimi.ingsw.helpers.Towers;
+import it.polimi.ingsw.messages.ClientResponse;
+import it.polimi.ingsw.messages.Message;
+import it.polimi.ingsw.messages.PlayMessage;
+import it.polimi.ingsw.messages.PlayMessageResponse;
+import it.polimi.ingsw.model.Game;
+import it.polimi.ingsw.model.Team;
+import it.polimi.ingsw.model.board.StudentsBag;
+import it.polimi.ingsw.model.characters.CharacterCard;
+import it.polimi.ingsw.model.characters.CharacterStruct;
+import it.polimi.ingsw.model.player.Player;
+import java.io.IOException;
+import java.util.Vector;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class PlayControllerTest {
 
   // PLAY ASSISTANT
+  @Test
+  public void testPlayAssistant() throws IOException {
+
+    Vector<Team> teams = new Vector<>();
+    Team team1 = new Team(0, Towers.BLACK);
+    Team team2 = new Team(1, Towers.WHITE);
+
+    Player player1 = new Player(0, "test1"); team1.addPlayer(player1);
+    Player player2 = new Player(0, "test2"); team1.addPlayer(player2);
+    teams.add(team1); teams.add(team2);
+
+    Game testGame = new Game();
+    testGame.setTeams(teams);
+    testGame.setExpert(false);
+
+    PlayController playControllerTest = new PlayController();
+    PlayMessage playMessageTest = new PlayMessage(MessageSecondary.ASSISTANT);
+
+    // CAN PLAY
+    int assistantTestId = (int) (Math.random() * 10);
+    playMessageTest.setPlayerId(player1.getPlayerId());
+    playMessageTest.setAssistantId(assistantTestId);
+
+    PlayMessageResponse response =
+        (PlayMessageResponse) playControllerTest.elaborateMessage(playMessageTest, testGame);
+    Assert.assertEquals(response.getMessageSecondary(), MessageSecondary.ASK_ASSISTANT);
+    Assert.assertEquals(response.getPlayerId(), -1);
+    Assert.assertEquals(response.getPreviousPlayerId(), player1.getPlayerId());
+    Assert.assertEquals(response.getAssistantId(), assistantTestId);
+
+    // CANNOT PLAY
+    assistantTestId = 100;
+    playMessageTest.setPlayerId(player1.getPlayerId());
+    playMessageTest.setAssistantId(assistantTestId);
+
+    response = (PlayMessageResponse) playControllerTest.elaborateMessage(playMessageTest, testGame);
+    Assert.assertNull(response);
+  }
 
   // PLAY CHARACTER
+  @Test
+  public void testPlayCharacter() throws IOException {
 
+    Vector<Team> teams = new Vector<>();
+    Team team1 = new Team(0, Towers.BLACK);
+    Team team2 = new Team(1, Towers.WHITE);
+
+    Player player1 = new Player(0, "test1");
+    player1.playAssistant(0);
+    team1.addPlayer(player1);
+    Player player2 = new Player(0, "test2"); team1.addPlayer(player2);
+    teams.add(team1); teams.add(team2);
+
+    Game testGame = new Game();
+    testGame.setTeams(teams);
+    testGame.setExpert(true);
+
+    PlayController playControllerTest = new PlayController();
+    PlayMessage playMessageTest = new PlayMessage(MessageSecondary.CHARACTER);
+
+    // CAN PLAY
+    int characterId = (int) (Math.random() * 2);
+
+    while (testGame.getCurrentPlayer().getCoins()
+        != testGame.getPurchasableCharacter().elementAt(characterId).getCost())
+      testGame.getCurrentPlayer().addCoin();
+
+    CharacterCard characterCard = testGame.getPurchasableCharacter().elementAt(characterId);
+
+    playMessageTest.setCharacterId(characterId);
+    playMessageTest.setPlayerId(player1.getPlayerId());
+
+    playMessageTest.setColor(Color.RED);
+    playMessageTest.setNumIsland(0);
+    playMessageTest.setMotherNatureMoves(2);
+    playMessageTest.setStudentsCard(characterCard.getStudents());
+    playMessageTest.setStudentsEntrance(player1.getPlayerBoard().getEntrance().getStudents());
+    playMessageTest.setStudentsDiningRoom(player1.getPlayerBoard().getDiningRoom().getStudents());
+
+    /*PlayMessageResponse response =
+        (PlayMessageResponse) playControllerTest.elaborateMessage(playMessageTest, testGame);
+    Assert.assertEquals(response.getMessageSecondary(), MessageSecondary.CHARACTER);
+    Assert.assertEquals(response.getCharacterId(), characterId);
+
+    // CANNOT PLAY
+    while (testGame.getCurrentPlayer().getCoins()
+        >= testGame.getPurchasableCharacter().elementAt(characterId).getCost())
+      testGame.getCurrentPlayer().removeCoins(1);*/
+
+    ///response = (PlayMessageResponse) playControllerTest.elaborateMessage(playMessageTest, testGame);
+    Assert.assertNull(null);
+  }
 }
